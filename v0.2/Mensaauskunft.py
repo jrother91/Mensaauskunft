3bd8c0792de3119ec275d8db278418285e1f6511
ronja <ronja.utescher@online.de>
2017-09-06 15:40:13 +0200

    added the correct v0.2 script

--- v0.2/Mensaauskunft.py
+++ v0.2/Mensaauskunft.py
@@ -22,8 +22,31 @@ def new_game():
 
     welcome_msg = render_template('welcome')
 
-    return statement(welcome_msg)
+    query = render_template('query')
+
+    return question(welcome_msg +' '+ query)
+
+
+@ask.intent("Done")
+
+def say_no():
+
+    goodbye = render_template('bye')
+
+    return statement(goodbye)
+
+
+@ask.intent("AskPrice")
+
+def state_price():
 	
+    price = render_template('price')
+
+    query = render_template('query')
+
+    return question(price +' '+ query)
+
+    
 if __name__ == '__main__':
 
     app.run(debug=True)
