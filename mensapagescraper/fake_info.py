9c888cb194a9ad8d991e87c77d73a90c965450e3
Jan-Eric Rother <jan-eric.rother@gmx.de>
2017-09-07 12:02:08 +0200

    added dummy mensa info

---
+++ mensapagescraper/fake_info.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+def get_mensa():
+    mensa_info = {"2017-09-07":{"main_menu":"Cordon Bleu",
+                                "veg_menu":"Kartoffelpuffer",
+                                "side_dishes":"Kartoffeln Erbsensuppe",
+                                "price":["2,60 €","4,50 €","5 €"]}}
+    return mensa_info
